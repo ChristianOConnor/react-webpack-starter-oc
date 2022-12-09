ca9a2c4d483ada179b6400df5073c84caf7bbaf8
ChristianOConnor <warpanomaly@gmail.com>
2022-12-09 17:43:17 -0500

    rm generator w/ assets filepath in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,7 @@ module.exports = {
         // Assets loader
         // More information here https://webpack.js.org/guides/asset-modules/
         test: /\.(gif|jpe?g|tiff|png|webp|bmp|svg|eot|ttf|woff|woff2)$/i,
-        type: 'asset',
-        generator: {
-          filename: 'assets/[hash][ext][query]',
-        },
+        type: 'asset'
       },
     ],
   },
